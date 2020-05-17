451b8858ed1a01bf5b7cd1276646d67734899aa6
henry lee <djteski@gmail.com>
2020-05-16 20:28:50 -0500

    Several things added

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const express = require('express')
+const express = require('express');
+const db = require('./lib/db');
+const validate = require('./lib/validate');
 
 
-const app = express;
-const port = 7890
+const app = express();
+const port = 7000;
+
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.set('view engine', 'hbs');
+
 
 
 app.listen(port, () => {

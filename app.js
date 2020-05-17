8b1cf0322273ecef4b30fc9f54f2cb42f7e3c0e9
henry lee <djteski@gmail.com>
2020-05-17 16:07:00 -0500

    views folder and README file added. app.js updated

--- app.js
+++ app.js
@@ -6,11 +6,23 @@ const validate = require('./lib/validate');
 const app = express();
 const port = 7000;
 
-app.use(express.static('public'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
 
 app.set('view engine', 'hbs');
 
+
+
+// Homepage showing the main lists
+app.get('/', (req, res) => {
+    db.getLists()
+        .then((lists) => {
+            res.render('index', {lists: lists})
+        })
+        .catch((err) => {
+            res.status(400).send('Could not find the lists')
+        })
+});
 
 
 app.listen(port, () => {
